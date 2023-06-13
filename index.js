d2b816e415d5e3499c4c48a0b44ec528182a59c5
Arthur Vinicius <thuur.vss@gmail.com>
2023-06-13 19:27:04 -0300

    feat: comentários para explicar o código

--- index.js
+++ index.js
@@ -5,24 +5,25 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
+// criação do cliente do whatsapp
 const client = new Client();
 
+// função para gerar o QR Code
 client.on("qr", (qr) => {
   qrcode.generate(qr, { small: true });
 });
 
+// função para quando o cliente estiver pronto
 client.on("ready", () => {
   console.log("Client is ready!");
 });
 
-client.on("message", (message) => {
-  if (message.from !== "status@broadcast") {
-    console.log(message);
-  }
-});
-
+// lista com telefones que podem enviar mensagens para o bot, podem ser adicionados mais números.
+// para da cada número, adicionar o número seguido de @c.us
+// caso deseje que qualquer número possa enviar mensagens, basta remover a linha da validação do número dentro do if
 const list_of_contacts = [`${process.env.CELL_NUMBER1}@c.us`];
 
+// função para quando o cliente receber uma mensagem, processar e enviar uma resposta usando o GPT-3
 client.on("message", async (message) => {
   if (
     message.type === "chat" &&
@@ -36,4 +37,5 @@ client.on("message", async (message) => {
   }
 });
 
+// inicialização do cliente
 client.initialize();
